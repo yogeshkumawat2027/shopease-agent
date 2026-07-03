96346b78eb3ebd2e855d266baf66fe4956327d8c
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-07-03 11:24:27 +0530

    initialize project with Express server and package.json installed required packages

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "ShopEase AI Agent API is running",
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
